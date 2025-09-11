5880f6b7c558f82cb33db007f76de8b2b30d3ec1
fwehn <finn.wehn@web.de>
2025-09-11 15:27:06 +0200

    Simple demo docker image;

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,38 +1,52 @@
 import vue from '@vitejs/plugin-vue';
 import autoprefixer from 'autoprefixer';
 import laravel from 'laravel-vite-plugin';
+import i18n from 'laravel-vue-i18n/vite';
+import { resolve } from 'node:path';
 import path from 'path';
 import tailwindcss from 'tailwindcss';
-import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
-import i18n from 'laravel-vue-i18n/vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return defineConfig({
+        server: {
+            // host: '0.0.0.0',
+            allowedHosts: true,
+            cors: {
+                origin: /.*/,
             },
-        }),
-        i18n(),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './resources/js'),
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+            hmr: {
+                host: env.VITE_HMR_HOST || 'localhost',
+            },
         },
-    },
-    css: {
-        postcss: {
-            plugins: [tailwindcss, autoprefixer],
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                ssr: 'resources/js/ssr.ts',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+            i18n(),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './resources/js'),
+                'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+            },
         },
-    },
-});
+        css: {
+            postcss: {
+                plugins: [tailwindcss, autoprefixer],
+            },
+        },
+    });
+};
